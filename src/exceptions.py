0338551345cae4f3f96a537b994955cf612b9ce6
Yhojan Mendoza <yhojanmendoza1@gmail.com>
2026-09-10 09:40:15 -0400

    refactor:

--- src/exceptions.py
+++
@@ -1,7 +0,0 @@
-class MalformedExpressionError(Exception):
-    """Excepción lanzada cuando la expresión matemática tiene errores de sintaxis."""
-    pass
-
-class DivisionByZeroTreeError(Exception):
-    """Excepción lanzada al intentar dividir por cero durante la evaluación del árbol."""
-    pass
